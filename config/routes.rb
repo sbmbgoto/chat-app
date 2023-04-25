31c8ee2d05a006c126cccdc4607e7c4228076639
sbmbgoto <syokusyoku6464@gmail.com>
2023-04-25 16:57:42 +0900

    メッセージの表示1

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
+<<<<<<< Updated upstream
   resources :rooms, only: [:new, :create]
+=======
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
+>>>>>>> Stashed changes
 end
